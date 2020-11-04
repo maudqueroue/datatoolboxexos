06c5150528cd901311fb55bfe6cc6d366767f524
Maud QUEROUE <maud.queroue@gmail.com>
2020-11-04 18:11:54 +0100

    :llama: Exercice drake

--- R/plan.R
+++ R/plan.R
@@ -11,9 +11,18 @@ write_plan <- function() {
     sp_eco = data_mammals_ecoregions(),
     sp_list = data_mammals(),
     pantheria = data_pantheria(),
+    # wrangle data
     ursus = extract_ursidae(sp_list),
     ursus_eco = join_data(ursus, sp_eco, eco_list),
     ursus_neco = get_necoregions(ursus_eco),
-    pan_tidy = tidy_pantheria(pantheria)
+    pan_tidy = tidy_pantheria(pantheria),
+    # figures
+    fig1 = plot_necoregions(ursus_neco),
+    fig2 = plot_gestation(pan_tidy),
+    fig3 = get_worldmap(ursus_eco),
+    # output
+    out_fig1 = save_plot(fig1, "figure1_necoregions"),
+    out_fig2 = save_plot(fig2, "figure2_gestation"),
+    out_fig3 = save_plot(fig3, "figure3_worldmap")
   )
 }
